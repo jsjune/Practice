c53d2ffb739c85e22ed469ddd0e320ad0b924e73
jsjune <avraskio@gmail.com>
2022-01-06 01:36:18 +0900

    initial commit

---
+++ src/java_100/Practice_1.java
@@ -0,0 +1,7 @@
+package java_100;
+
+public class Practice_1 { // 접근제한자 클래스선언 클래스이름
+	public static void main(String[] args) { // 접근제한자 static 반환타입 메서드이름(파라미터){
+		System.out.println("Hello World"); // 구현할 코드 작성
+	}
+}
