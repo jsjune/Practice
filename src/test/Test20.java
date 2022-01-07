ca8f53eb922222e41bbbdf67a2a50ffa5d36aff2
jsjune <avraskio@gmail.com>
2022-01-07 17:21:27 +0900

    initial commit

--- src/test/Test20.java
+++ src/test/Test20.java
@@ -13,10 +13,30 @@ class Car{
 	}
 	public void setCar(int n, double g)
 	{
-		num = n;
+		num = n; gas = g;
+		System.out.println("차량 번호를" + num + "으로, 연료량을" + gas + "로 바꾸었습니다.");
+	}
+	public static void showSum() { // 클래스 메소드 이다.
+		System.out.println("자동차는 모두" + sum + "대 있습니다.");
+	}
+	public void show() {
+		System.out.println("차량 번호는" + num + "입니다.");
+		System.out.println("연료량은" + gas + "입니다.");
 	}
 }
 
 public class Test20 {
-
+	public static void main(String[] args) {
+		Car.showSum();
+		
+		Car car1 = new Car(); // 객체를 생성
+		car1.setCar(1234, 20.5);
+		
+		Car.showSum(); // 다시 한번 메소드를 호출
+		
+		Car car2 = new Car();
+		car2.setCar(4567, 30.5);
+		
+		Car.showSum();
+	}
 }
