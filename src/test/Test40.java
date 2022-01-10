4cf6e14bd0f6c7154a54e7674270e13b693e7242
jsjune <avraskio@gmail.com>
2022-01-10 17:47:35 +0900

    initial commit

---
+++ src/test/Test40.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Test40 {
+	public static void main(String[] args) {
+		Test40Car car1 = new Test40Car();
+		car1.show();
+	}
+}
---
+++ src/test1/Test40.java
@@ -0,0 +1,8 @@
+package test1;
+
+public class Test40 {
+	public static void main(String[] args) {
+		test.Test40Car car1 = new test.Test40Car();
+		car1.show();
+	}
+}
